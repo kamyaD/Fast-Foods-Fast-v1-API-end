731bdfef8b72e65f4dd8072ebaa6ef15160190be
Kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-19 00:04:35 +0300

    Update firstfood_test.py

--- firstfood_test.py
+++ firstfood_test.py
@@ -1,79 +1,40 @@
-import unittest
 
+#import objects from the Flask model
+from flask import Flask, render_template, jsonify, request,session,flash,redirect,url_for 
 
-# local imports
-from firstfood import orders
-from firstfood import app 
+app = Flask(__name__, template_folder='v1') #define app and telling flask that template folder is named v1
+orders = [{'name':'coffee'}, {'name':'Beef'},{'name' : 'Milk'}] # Making a Dictionary of orders that is to be used to test the code
 
+@app.route('/api/v1/order', methods=['GET']) #Testing the jsonify out put on a browser
+def output():
+    return jsonify({'message' : 'Itworks!'})
 
-class TestapiEndpoints(unittest.TestCase):
+@app.route('/api/v1/all_orders', methods=['GET']) # GET API that gets all orders
+def returnAll():
+    return jsonify({'orders': orders})
 
-    # ensuring jsonify returns the right output
-    def test_output(self):
-        self.client = app.test_client(self)
-        self.data={'message' : 'Itworks!'}
-        self.output = self.client.get(
-            '/api/v1/order', data=self.data)
-        self.assertEqual(self.output.status_code, 200)
-        
+@app.route('/api/v1/all_orders/<string:name>', methods=['GET']) # fetch Specific order
+def returnOne(name):
+    ords=[order for order in orders if order['name']== name]
+    return jsonify({'order' : ords[0]})
 
-    # Ensure All orders orders are returned
-    def test_returnAll(self):
-        self.client = app.test_client(self)
-        self.output = self.client.get(
-            '/api/v1/all_orders', content_type='html/text')
-        self.assertEqual(self.output.status_code, 200)
+@app.route('/api/v1/all_orders', methods=['POST']) # Places a new Order
+def addOrder():
+    order = request.get_json('name')
+    orders.append(order)
+    return jsonify({'orders' : orders})
 
-    # Ensure one order is returned
-    def test_returnOne(self):
-        self.client = app.test_client(self)
-        self.order= "Beef"
-        self.ords=[order for order in orders if order['name']== self.order]
-        return self.order       
-        self.output=self.client.get(
-            '/api/v1/all_orders/<string:name>', data=self.ords)
-        self.assertEqual(self.output.status_code, 200)
+@app.route('/api/v1/all_orders/<string:name>', methods=['PUT']) # Update the status of an order
+def editOrder(name):
+    ords=[order for order in orders if order['name']== name]
+    ords[0]['name'] = request.get_json(['name'])
+    return jsonify({'order' : ords[0]})
 
-        
+@app.route('/api/v1/all_orders/<string:name>', methods=['Delete']) # Delete an order
+def deleteOrder(name):
+    delOrder=[order for order in orders if order['name']== name]
+    orders.remove(delOrder[0])
+    return jsonify({'orders': orders})
 
-    # Ensure that new order is created
-    def test_addOrder(self):
-        self.client = app.test_client(self)
-        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
-        self.order= {'name':'Bread'}
-        self.new_orders=orders.append(self.order)
-        self.output = self.client.post(
-            '/api/v1/all_orders', data = self.new_orders)
-        self.assertTrue(self.output.status_code, 201 )
-
-    
-    # Ensure status of an order is Updated
-    def test_editOrder(self):
-        self.client = app.test_client(self)
-        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
-        self.order= {'name':'Tea'}
-        self.new_orders= [order for order in orders if order['name']== ['coffee']]
-        self.orders[0]['name'] = self.order
-        self.output = self.client.put(
-            '/api/v1/all_orders' , data = self.new_orders)
-        self.assertTrue(self.output.status_code, 201)
-
-        
-    # Ensure  an order is deleted
-    def test_deleteOrder(self):
-        self.client = app.test_client(self)
-        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
-        self.new_orders=[order for order in orders if order['name']== ['coffee']]
-        self.orders.remove(self.orders[0])
-        return self.new_orders
-        self.output = self.client.delete(
-            '/api/v1/all_orders' , data = self.new_orders)
-        self.assertTrue(self.output.status_code, 200)
-        self.assertEqual(self.new_orders,[{'name':'Beaf'},{'name' : 'Milk'}])
-    
-    
-   
-   
 if __name__ == '__main__':
-    unittest.main()
-
+    app.run(debug=False)
