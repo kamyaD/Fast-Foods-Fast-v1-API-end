fbaec43bb58f62bb9ba6669c268ca69a9dad0aec
Kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-19 00:04:04 +0300

    Update firstfood_test.py

--- firstfood_test.py
+++ firstfood_test.py
@@ -1,73 +1,79 @@
 import unittest
 
-#local imports
+
+# local imports
 from firstfood import orders
 from firstfood import app 
 
 
-class apiEndpoints(unittest.TestCase):
-    
-    
-    # Ensure that flask is working
-    def test_welcome(self):
-        client = app.test_client(self)
-        output = client.get('/v1', content_type='html/text')
-        self.assertEqual(output.status_code, 200)
-        self.assertIn(b'Welcome to Farst Foods Farst!.Please Sign up:', output.data)
+class TestapiEndpoints(unittest.TestCase):
+
+    # ensuring jsonify returns the right output
+    def test_output(self):
+        self.client = app.test_client(self)
+        self.data={'message' : 'Itworks!'}
+        self.output = self.client.get(
+            '/api/v1/order', data=self.data)
+        self.assertEqual(self.output.status_code, 200)
         
+
     # Ensure All orders orders are returned
     def test_returnAll(self):
-        client = app.test_client(self)
-        output = client.get(
+        self.client = app.test_client(self)
+        self.output = self.client.get(
             '/api/v1/all_orders', content_type='html/text')
-        self.assertEqual(output.status_code, 200)
+        self.assertEqual(self.output.status_code, 200)
+
+    # Ensure one order is returned
+    def test_returnOne(self):
+        self.client = app.test_client(self)
+        self.order= "Beef"
+        self.ords=[order for order in orders if order['name']== self.order]
+        return self.order       
+        self.output=self.client.get(
+            '/api/v1/all_orders/<string:name>', data=self.ords)
+        self.assertEqual(self.output.status_code, 200)
+
         
 
     # Ensure that new order is created
     def test_addOrder(self):
-        client = app.test_client(self)
+        self.client = app.test_client(self)
         self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
         self.order= {'name':'Bread'}
         self.new_orders=orders.append(self.order)
-        output = client.post(
+        self.output = self.client.post(
             '/api/v1/all_orders', data = self.new_orders)
-        self.assertTrue(output.status_code, 201 )
+        self.assertTrue(self.output.status_code, 201 )
 
     
     # Ensure status of an order is Updated
     def test_editOrder(self):
-        client = app.test_client(self)
+        self.client = app.test_client(self)
         self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
         self.order= {'name':'Tea'}
         self.new_orders= [order for order in orders if order['name']== ['coffee']]
         self.orders[0]['name'] = self.order
-        output = client.put(
+        self.output = self.client.put(
             '/api/v1/all_orders' , data = self.new_orders)
-        self.assertTrue(output.status_code, 201)
+        self.assertTrue(self.output.status_code, 201)
 
+        
     # Ensure  an order is deleted
     def test_deleteOrder(self):
-        client = app.test_client(self)
+        self.client = app.test_client(self)
         self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
         self.new_orders=[order for order in orders if order['name']== ['coffee']]
         self.orders.remove(self.orders[0])
         return self.new_orders
-        output = client.delete(
+        self.output = self.client.delete(
             '/api/v1/all_orders' , data = self.new_orders)
-        self.assertTrue(output.status_code, 200)
+        self.assertTrue(self.output.status_code, 200)
         self.assertEqual(self.new_orders,[{'name':'Beaf'},{'name' : 'Milk'}])
     
-    def test_login(self):
-        client = app.test_client(self)
-        output = client.post(
-            '/v1/login',
-            data=dict(username="domnic", password="domnic"),
-            follow_redirects=True 
-        )
-        self.assertIn(b'Please Make your order', output.data)
-
-
-
+    
+   
+   
 if __name__ == '__main__':
     unittest.main()
 
